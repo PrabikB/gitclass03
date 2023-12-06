f427e755cb9570cd547c84a294c3b240bb043feb
PrabikB <baral.bikal@gmail.com>
2023-12-05 20:07:18 -0500

    second commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,5 +3,9 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello Batch 18!");
+        System.out.println("Hello batch 18!");
+        System.out.println("Hello batch 18!");
+
+
     }
 }
